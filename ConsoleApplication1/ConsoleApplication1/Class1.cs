c20fa060d97e5c9695e2e817aa8c02430aeb0689
Patryk Stulczewski <patrykj122@o2.pl>
2015-06-13 09:19:57 +0200

    test1

---
+++ ConsoleApplication1/ConsoleApplication1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        public void spiewaj(int x)
+        {
+            System.Console.WriteLine("dsadas" + x);
+        }
+    }
+}
